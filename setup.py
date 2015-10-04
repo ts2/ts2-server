f264239e119e29e0d0b3b90c60bca1cccb4eab98
Pedro Morgan <pedromorgan@gmail.com>
2015-10-04 01:18:23 +0100

    test live

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from setuptools import setup
 
-setup(name='YourAppName',
-      version='1.0',
-      description='OpenShift App',
-      author='Your Name',
-      author_email='example@example.com',
-      url='https://www.python.org/community/sigs/current/distutils-sig',
+setup(name='ts2-server',
+      version='0.1',
+      description='TS2 OpenShift App',
+      author='Pedro Morgan',
+      author_email='ts2@daffodil.uk.com',
+      url='http://ts2.github.io/',
       install_requires=['Flask>=0.7.2', 'MarkupSafe'],
      )
---
+++ wsgi/ts2.py
@@ -0,0 +1,45 @@
+from flask import Flask, jsonify, request
+from flask.ext.sqlalchemy import SQLAlchemy
+
+
+
+# temp hack to load etc
+ALLOWED_IPS = [
+    "127.0.0.1"
+]
+
+app = Flask(__name__)
+db = SQLAlchemy(app)
+
+
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True)
+    email = db.Column(db.String(120), unique=True)
+
+    def __init__(self, username, email):
+        self.username = username
+        self.email = email
+
+    def __repr__(self):
+        return '<User %r>' % self.username
+
+
+
+@app.route("/")
+def index():
+    return jsonify(info="Hello TS2!", ip=request.remote_addr)
+
+
+@app.route("/db/create")
+def db_create():
+    db.create_all()
+    return jsonify(success=True)
+
+## Run Local
+if __name__ == "__main__":
+    app.debug = True
+    app.run()
+
